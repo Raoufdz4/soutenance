99db4e5dba8a1696fcaad8d62a7d4e17e8b04433
ayoub ouadah <ayoubouadah.w@gmail.com>
2023-12-26 10:05:52 +0100

    aaa

--- icon.php
+++ icon.php
@@ -3,6 +3,6 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="<?php echo $dist ?>/img/colivraison.ico">
+  <link rel="icon" href="<?php echo $dist ?>/ServerData/img/colivraison.ico">
 </head>
  
