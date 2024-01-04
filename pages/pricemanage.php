a73ab843759d26a552d52155f7bffa20738169cc
ayoub ouadah <ayoubouadah.w@gmail.com>
2024-01-04 13:14:20 +0100

    546546

--- pages/pricemanage.php
+++ pages/pricemanage.php
@@ -101,43 +101,41 @@ include $partials.'header_left.php';
 <div class="card">
 <div class="card-body">
 <div class="row d-flex justify-content-lg-center mt-1 ml-1 mr-1">
-<div class="col-12 bg-gray-light m-1 rounded">
 
-<div class="text-muted mt-3 ml-2  ">Select Case :</div>
-<hr>
-  <div class="row">
-    <div class="col-12 ml-2">
-    <label class="ml-2" for="country">Available cases :</label>
-    </div>
-    </div>
-    <div class="row mb-4">
-      <div class="col-11 ml-3">
-      <select class="form-control bfh-countries" name="cases" id="cases" >
-                    <option>The best Expectations</option>
-                    <option>The usual Expectations</option>
-                    <option>The worst Expectations</option>
-                    </select>
-      </div>  
-    </div>
-    
-  </div>  
 
   <div class="col-12 bg-gray-light m-1 rounded">
 
 <div class="text-muted mt-3 ml-2  ">Calcule :</div>
 <hr>
+
     <div class="row mb-4">
       <div class="col-11 ml-3">
-      <label for="">Product price:</label>
-      <input type="text">
-        <label for="">Ads euro</label>
-      <input type="text">
-      <label for="">Ads da</label>
-      <input type="text">
-      <label for="">CPR%</label>
-      <input type="text">
-      <label for="">DR%</label>
-      <input type="text">
+        <div class="row">
+            <div class="col-4">
+            <label for="">Product price:</label>
+            <input type="number" id="price_presale" onkeyup="calculate()">
+            </div>
+        <div class="col-4">
+        <label for="">Ads da</label>
+        <input type="number" id="price_adsdz"  onkeyup="calculate()">
+        </div>
+      
+        </div>
+      <div class="row">
+        <div class="col-4">
+        <label for="">CPC%</label>
+      <input type="number"  id="cpc"  onkeyup="calculate()">
+        </div>
+        <div class="col-4">
+        <label for="">CPD%</label>
+        <input type="number" id="cpd"  onkeyup="calculate()">
+        </div>
+        <div class="col-4">
+        <label for="">Sale price</label>
+      <input type="number" id="price_sale"  onkeyup="calculate()">
+        </div>
+      
+      </div>
       </div>  
       
     </div>
@@ -145,9 +143,8 @@ include $partials.'header_left.php';
         <div class="col-10">
 
         </div>
-        <div class="col-2 d-flex justify-content-center">
-            <button class="btn btn-primary file-input-label pl-4 pr-4">Calculate</button>
-        </div>
+        <p id="result1">totalprice: </p>
+        <p id="result2">Result: </p>
     </div>
   </div>  
 </div>
@@ -182,7 +179,43 @@ include $partials.'header_left.php';
 
 
 
+<script>
+    var price_presale = 0;
+    var price_adsdz = 0;
+    var cpc = 0;
+    var cpd = 0;
+    var price_sale = 0;
+    var totalprice = 0;
+    var profit = 0;
 
+function calculate() {
+ price_presale = parseFloat(document.getElementById("price_presale").value);
+ price_adsdz = parseFloat(document.getElementById("price_adsdz").value);
+ cpc = parseFloat(document.getElementById("cpc").value);
+ cpd = parseFloat(document.getElementById("cpd").value);
+ price_sale = parseFloat(document.getElementById("price_sale").value);
+
+if (!isNaN(price_presale) && !isNaN(price_adsdz) && !isNaN(cpc) && !isNaN(cpd)) {
+                // Perform the addition
+                var cpc_clc = (price_adsdz * 100)/cpc
+                var cpd_clc =  (cpc_clc * 100)/cpd
+                var livraison = 100
+                var stock= 100
+                var returnn = 0
+                totalprice = price_presale + cpd_clc + livraison + stock + returnn
+                
+                // Display the result
+                document.getElementById("result1").innerText = "totalprice: " + totalprice;
+                if (!isNaN(price_sale)) {
+                    profit = price_sale - totalprice
+                    document.getElementById("result2").innerText = "profit: " + profit;
+                }
+                
+            }
+
+
+}
+</script>
 
 
 
