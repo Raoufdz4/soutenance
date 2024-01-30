45e6a7e2c83f08941f63b435d68f04d85a6e6ccb
ayoub ouadah <ayoubouadah.w@gmail.com>
2024-01-30 11:36:14 +0100

    fdgdfg

--- pages/user/cases.php
+++ pages/user/cases.php
@@ -60,7 +60,7 @@ include $partials.'header_left.php';
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
-
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <body class="sidebar-mini layout-fixed">
 <div class="wrapper">
 
@@ -114,17 +114,19 @@ include $partials.'header_left.php';
     </div>
     <div class="row mb-4">
       <div class="col-9 ml-3">
-      <select class="form-control bfh-countries" name="cases" id="cases" >
+      <select class="form-control bfh-countries" name="cases" id="cases">
+        <option value="*" selected="selected">Select case</option>
         <?php 
         $req="SELECT * FROM cases WHERE cases_user='ALL' OR cases_user='$useremail'";
         $res=mysqli_query($cnx,$req);
         if ($res) {
           while ($row=mysqli_fetch_assoc($res)) {
-            echo "<option>".$row['case_name']."</option>";
+            echo '<option value="'.$row['id_case'].'">'.$row['case_name'].'</option>';
           }
         }
         ?>
                     </select>
+
       </div>  
                     <div class="col-2 ml-4 d-flex justify-content-end">
     <button class="btn btn-primary file-input-label pl-4 pr-4">Add case</button>
@@ -151,10 +153,9 @@ include $partials.'header_left.php';
 <hr>
   <div class="row">
     <div class="col-12 ml-2">
-    <p>- Ads euro : 1</p>
-    <p>- Ads da : 230</p>
-    <p>- CPR% : 60</p>
-    <p>- DR% : 50</p>
+    <div id="caseDetails">
+    <!-- Display data here -->
+  </div>
     </div>
     </div>
    
@@ -204,7 +205,56 @@ include $partials.'footer.php';
 ?>
 
 </div>
+<script>
+    $(document).ready(function() {
+    // Attach an event listener to the select element
+    $('#cases').change(function() {
+        // Get the selected option value
+        var selectedCaseId = $(this).val();
 
+        // Use AJAX to fetch data from the server
+        $.ajax({
+            type: 'POST',
+            url: '../php_handling/casesoption.php',
+            data: { cases: selectedCaseId },
+            dataType: 'json', // Specify that the expected response is JSON
+            success: function(response) {
+                // Check if the response contains an error
+                if (response.error) {
+                    console.error('Error fetching data: ' + response.error);
+                } else {
+                    // Process the response data
+                    if (response.length > 0) {
+                        var firstDataItem = response[0];
+                        console.log('Ads Euro:', firstDataItem.ads_euro);
+                        console.log('Exchange Rate:', firstDataItem.exchange_rate);
+                        console.log('CPC:', firstDataItem.cpc);
+                        console.log('CPD:', firstDataItem.cpd);
+                        // ... process other properties
+
+                        // Update the content of the caseDetails div with the fetched data
+                        // Modify this part according to your HTML structure
+                        $('#caseDetails').html('<p>Ads Euro: ' + firstDataItem.ads_euro + '</p>');
+                        $('#caseDetails').append('<p>Exchange Rate: ' + firstDataItem.exchange_rate + '</p>');
+                        $('#caseDetails').append('<p>CPC: ' + firstDataItem.cpc + ' %</p>');
+                        $('#caseDetails').append('<p>CPD: ' + firstDataItem.cpd + ' %</p>');
+                        // ... append other properties
+                    } else {
+                        console.log('No data returned from the server.');
+                        // You might want to clear the content of #caseDetails in this case
+                        $('#caseDetails').html('');
+                    }
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error('AJAX error: ' + textStatus, errorThrown);
+            }
+        });
+    });
+});
+
+  </script>
+  
 <?php
 include  '../../links/js.php';
 ?>
