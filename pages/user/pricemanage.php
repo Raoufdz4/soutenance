37adedc7122e68eabf1e1926507ab36ffb91aa4e
ayoub ouadah <ayoubouadah.w@gmail.com>
2024-02-03 10:23:47 +0100

    rtyrtyrt

--- pages/user/pricemanage.php
+++ pages/user/pricemanage.php
@@ -95,6 +95,45 @@ include $partials.'header_left.php';
       </div>
     </div>
 
+    <div class="row">
+<div class="col-lg-12">
+<div class="card">
+<div class="card-body">
+<div class="row d-flex justify-content-lg-center mt-1 ml-1 mr-1">
+<div class="col-12 bg-gray-light m-1 rounded">
+
+<div class="text-muted mt-3 ml-2  ">Cases :</div>
+<hr>
+  <div class="row">
+    <div class="col-12 ml-2">
+    <label class="ml-2" for="country">Available cases :</label>
+    </div>
+    </div>
+    <div class="row mb-4">
+      <div class="col-11 ml-3 ">
+      <select class="form-control bfh-countries" name="cases" id="cases">
+        <option value="*" selected="selected">Select case</option>
+        <?php 
+        $req="SELECT * FROM cases WHERE cases_user='ALL' OR cases_user='$useremail'";
+        $res=mysqli_query($cnx,$req);
+        if ($res) {
+          while ($row=mysqli_fetch_assoc($res)) {
+            echo '<option value="'.$row['id_case'].'">'.$row['case_name'].'</option>';
+          }
+        }
+        ?>
+                    </select>
+
+      </div>  
+    </div>
+    
+  </div>  
+
+</div>
+</div>
+</div>
+</div>
+</div>
 
     <div class="row">
 <div class="col-lg-12">
@@ -110,41 +149,61 @@ include $partials.'header_left.php';
 
     <div class="row mb-4">
       <div class="col-11 ml-3">
+
+
         <div class="row">
             <div class="col-4">
-            <label for="">Product price:</label>
+            <label for="price_presale">Product price :</label>
             <input type="number" id="price_presale" onkeyup="calculate()">
             </div>
+        </div>
+<hr>
+        <div class="row">
         <div class="col-4">
-        <label for="">Ads da</label>
-        <input type="number" id="price_adsdz"  onkeyup="calculate()">
+        <label for="price_adseuro">Ads euro :</label>
+        <input type="number" id="price_adseuro"  onkeyup="calculate()">
         </div>
-      
         </div>
+
       <div class="row">
         <div class="col-4">
-        <label for="">CPC%</label>
+        <label for="cpc">CPC% :</label>
       <input type="number"  id="cpc"  onkeyup="calculate()">
         </div>
-        <div class="col-4">
-        <label for="">CPD%</label>
+      </div>
+
+      <div class="row">
+      <div class="col-4">
+        <label for="cpd">CPD% :</label>
         <input type="number" id="cpd"  onkeyup="calculate()">
         </div>
+      </div>
+<hr>
+<div class="row">
+<p id="result1"  class="ml-5">Total price : </p>
+</div>
+<hr>
+        <div class="row">
         <div class="col-4">
         <label for="">Sale price</label>
       <input type="number" id="price_sale"  onkeyup="calculate()">
         </div>
-      
-      </div>
+        </div>
+        <hr>
+<div class="row">
+<p id="result2" class="ml-5">Profit : </p>
+</div>
+<hr>
       </div>  
-      
+
+
     </div>
     <div class="row mb-4">
         <div class="col-10">
 
         </div>
-        <p id="result1">totalprice: </p>
-        <p id="result2">Result: </p>
+        
+        
     </div>
   </div>  
 </div>
@@ -182,6 +241,7 @@ include $partials.'header_left.php';
 <script>
     var price_presale = 0;
     var price_adsdz = 0;
+    var price_adseuro = 0;
     var cpc = 0;
     var cpd = 0;
     var price_sale = 0;
@@ -190,30 +250,30 @@ include $partials.'header_left.php';
 
 function calculate() {
  price_presale = parseFloat(document.getElementById("price_presale").value);
- price_adsdz = parseFloat(document.getElementById("price_adsdz").value);
+ price_adseuro = parseFloat(document.getElementById("price_adseuro").value);
  cpc = parseFloat(document.getElementById("cpc").value);
  cpd = parseFloat(document.getElementById("cpd").value);
  price_sale = parseFloat(document.getElementById("price_sale").value);
 
-if (!isNaN(price_presale) && !isNaN(price_adsdz) && !isNaN(cpc) && !isNaN(cpd)) {
+if (!isNaN(price_presale) && !isNaN(price_adseuro) && !isNaN(cpc) && !isNaN(cpd)) {
                 // Perform the addition
+                var price_adsdz = price_adseuro * 230
                 var cpc_clc = (price_adsdz * 100)/cpc
                 var cpd_clc =  (cpc_clc * 100)/cpd
                 var livraison = 100
                 var stock= 100
                 var returnn = 0
                 totalprice = price_presale + cpd_clc + livraison + stock + returnn
-                
+                totalprice = (Math.round(totalprice * 100) / 100).toFixed(2);
                 // Display the result
-                document.getElementById("result1").innerText = "totalprice: " + totalprice;
+                document.getElementById("result1").innerText = "Total price : " + totalprice + " DA";
                 if (!isNaN(price_sale)) {
                     profit = price_sale - totalprice
-                    document.getElementById("result2").innerText = "profit: " + profit;
+                    profit = (Math.round(profit * 100) / 100).toFixed(2);
+                    document.getElementById("result2").innerText = "Profit : " + profit + " DA";
                 }
                 
             }
-
-
 }
 </script>
 
