5d6006f0bb86cb51cf51f313d8c1fbe3be906097
raoufdz4 <raoufattouche@gmail.com>
2024-02-13 05:24:29 +0100

    ..

--- pages/php_handling/admin_manageproduct.php
+++ pages/php_handling/admin_manageproduct.php
@@ -5,24 +5,48 @@ include '../../config.php';
 $useremail = $_SESSION['email'];
 
 $responseData = array();
+$productnum = array();
 
-if (isset($_POST['cases']) && !empty($_POST['cases'])) {
-    $caseid = $_POST['cases'];
+if (isset($_POST['dropdown']) && !empty($_POST['dropdown'])) {
+    $userid = $_POST['dropdown'];
 
-    $query = "SELECT * FROM cases WHERE id_case='$caseid' AND (cases_user='$useremail' OR cases_user='ALL')";
+    $query = "SELECT COUNT(*) as total FROM product WHERE product_user_id='$userid'";
     $result = mysqli_query($cnx, $query);
+
+    $query2 = "SELECT * FROM product WHERE 	product_user_id='$userid'";
+    $result2 = mysqli_query($cnx, $query2);
+
+    
+
 
     if ($result) {
         // Check if any rows were returned
         if (mysqli_num_rows($result) > 0) {
             // Fetch and store data in responseData array
             while ($row = mysqli_fetch_assoc($result)) {
-                $responseData[] = array(
-                    'ads_euro' => $row['ads_euro'],
-                    'exchange_rate' => $row['exchange_rate'],
-                    'cpc' => $row['cpc'],
-                    'cpd' => $row['cpd']
+                $productnum[] = array(
+                    'productnum' => $row['total']
                 );
+            }
+        } else {
+            // Handle case where no rows were returned
+            $productnum['error'] = 'No data found for the specified case ID.';
+        }
+    } else {
+        // Handle database query error
+        $productnum['error'] = 'Error executing database query: ' . mysqli_error($cnx);
+    }
+
+    if ($result2) {
+        // Check if any rows were returned
+        if (mysqli_num_rows($result2) > 0) {
+            // Fetch and store data in responseData array
+            $i=0;
+            while ($i<=$productnum['productnum']) {
+                $responseData[] = array(
+                    'id_produit ' => $row['id_produit ']
+                );
+                
             }
         } else {
             // Handle case where no rows were returned
